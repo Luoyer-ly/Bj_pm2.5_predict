b599d800982bae6cd7ffefb3ed25b3bbccd42980
lweihua <lweihua@gsta.com>
2019-02-15 18:34:53 +0800

    更改最后的激活函数，不要sigmoid要tanh

--- build_network.py
+++ build_network.py
@@ -1,11 +1,11 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def initialize_coef_deep(layers):
     layer_size = len(layers)
     parameters = {}
     for i in range(1, layer_size):
-        parameters["W" + str(i)] = np.random.randn(layers[i], layers[i - 1]) * 0.01
+        parameters["W" + str(i)] = np.ones((layers[i], layers[i - 1]), dtype=float)
         parameters["b" + str(i)] = np.zeros((layers[i], 1))
     return parameters
 
@@ -20,7 +20,7 @@ def forward_propagation(X, parameters):
         caches["A" + str(i)] = A
         caches["Z" + str(i)] = Z
     AL, ZL = linear_activation_forward(A, parameters["W" + str(layer_size)], parameters["b" + str(layer_size)],
-                                       "sigmoid")
+                                       "tanh")
     caches["A" + str(layer_size)] = AL
     caches["Z" + str(layer_size)] = ZL
     return AL, caches
@@ -49,12 +49,12 @@ def backward_propagation(parameters, AL, caches, Y):
     Y = Y.reshape(AL.shape)
     m = Y.shape[1]
     dAL = -np.divide(Y, AL) + np.divide(1 - Y, 1 - AL)
-    dAL = np.sum(dAL, axis=1, keepdims=True) / m
+    # dAL = np.sum(dAL, axis=1, keepdims=True) / m
     L = len(parameters) // 2
     grads = {}
     dA_previous, dW, db = linear_activation_backward(dAL, caches["A" + str(L - 1)], caches["Z" + str(L)],
                                                      parameters["W" + str(L)],
-                                                     parameters["b" + str(L)], "sigmoid")
+                                                     parameters["b" + str(L)], "tanh")
     grads["dW" + str(L)] = dW
     grads["db" + str(L)] = db
     for i in reversed(range(1, L)):
@@ -77,6 +77,8 @@ def linear_activation_forward(A_prev, W, b, activation):
         A = sigmoid(Z)
     elif activation == 'relu':
         A = relu(Z)
+    elif activation == 'tanh':
+        A = tanh(Z)
     return A, Z
 
 
@@ -86,6 +88,8 @@ def linear_activation_backward(dA, A_previous, Z, W, b, activation):
         dZ = sigmoid_backward(dA, Z)
     elif activation == "relu":
         dZ = relu_backward(dA, Z)
+    elif activation == 'tanh':
+        dZ = tanh_backward(dA, Z)
     dA_previous, dW, db = linear_backward(dZ, A_previous, W, b)
     return dA_previous, dW, db
 
@@ -124,3 +128,11 @@ def relu_backward(dA, Z):
     dZ = np.array(dA, copy=True)
     dZ[Z <= 0] = 0
     return dZ
+
+
+def tanh(A):
+    return np.divide(np.exp(A) - np.exp(-A), np.exp(A) + np.exp(-A))
+
+
+def tanh_backward(dA, Z):
+    return dA * (1 - np.power(tanh(Z), 2))
--- logistic_reg.py
+++ logistic_reg.py
@@ -29,21 +29,47 @@ with open(fileName) as f:
             train_result.append(row[2])
     f.close()
 X = np.array(train_data, dtype=float, copy=True).T
-Y = np.array(train_result[:], dtype=float)/1000.0
+X = np.divide(X, 1000.0)
+Y = np.array(train_result[:], dtype=float)
+Y = np.divide(Y, 100.0)
 Y = Y.reshape((1, Y.shape[0]))
 # print(max(Y[0,:]))
 
 layer_dims = [X.shape[0], 7, 1]
-parameters = L_layer_model(X, Y, layer_dims, learning_rate=0.0075, iteration=1000, print_cost=True)
+parameters = L_layer_model(X, Y, layer_dims, learning_rate=0.0075, iteration=2000, print_cost=True)
 
+# AL, caches = forward_propagation(X, parameters)
+# print(AL)
 
-AL, caches = forward_propagation(X, parameters)
+test_file = './pm25_test.csv'
+test_data = []
+with open(test_file) as t:
+    t_reader = csv.reader(t)
+    for row in t_reader:
+        if t_reader.line_num > 1:
+            tmp_data = []
+            for index in range(len(row)):
+                if index == 0:
+                    month_day = str(row[index]).split('-')
+                    # 转为日期类型
+                    date_type = datetime.datetime.strptime("".join(month_day), "%Y%m%d")
+                    # 获取在一年中的天数
+                    today = date_type.timetuple().tm_yday
+                    tmp_data.append(float(today))
+                else:
+                    tmp_data.append(float(row[index]))
+            test_data.append(tmp_data)
+    t.close()
+test_X = np.array(test_data, dtype=float, copy=True).T
+test_X = np.divide(test_X, 1000)
+assert test_X.shape[0] == X.shape[0]
+AL, ZL = forward_propagation(X, parameters)
 print(AL)
-
-out = open("res.csv", 'a', newline='')
+out = open("res.csv", 'w', newline='')
 csv_writer = csv.writer(out, dialect='excel')
-AL = np.multiply(AL, 1000)
+AL = np.multiply(AL, 100)
 AL = AL.T
+csv_writer.writerow(["pm2.5"])
 for i in range(len(AL)):
     csv_writer.writerow(AL[i])
 out.close()
